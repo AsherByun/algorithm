76ffcd167d19bc1037211d7261660c1caf94788a
변성재 <dfdo333@naver.com>
2020-12-30 22:52:44 +0900

    정사각형 값

--- 백준알고리즘풀이/algo1915.java
+++ 백준알고리즘풀이/algo1915.java
@@ -1,25 +1,50 @@
-package algo;
-import java.util.*;
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class algo1915 {
-	static int n,m;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
 
-		Scanner scan = new Scanner(System.in);
-		n=scan.nextInt();
-		m=scan.nextInt();
-		
-		
-		int map[][] = new int[n+1][m+1];
-		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=m;j++) {
-				map[i][j]=scan.nextInt();
-			}
-		}
-		
-		
-		
-	}
+        int n=Integer.parseInt(str[0]);
+        int m=Integer.parseInt(str[1]);
 
+        int[][] map = new int[n+1][m+1];
+        int[][] dp = new int[n+1][m+1];
+
+        for (int i=1;i<=n;i++){
+            str = br.readLine().split("");
+            for(int j=1;j<=m;j++){
+                map[i][j] = Integer.parseInt(str[j-1]);
+            }
+        }
+
+        int max = 0;
+
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+
+                if (map[i][j]==1) {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j - 1]), dp[i - 1][j])+1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+//        for (int i=0;i<=n;i++){
+//            for (int j=0;j<=m;j++){
+//                System.out.print(dp[i][j]+" ");
+//            }System.out.println();
+//        }
+
+        System.out.print(max*max);
+
+    }
 }
+/*
+    대각, 위, 옆중에 가장 작은값에서 +1
+    만약 0이있으면 1또는 0 이되면서 올바른 값을 뽑아낼수있다.
+
+ */
