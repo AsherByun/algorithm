d3abe924410945cc884c7d340b84b94c565fa86b
변성재 <dfdo333@naver.com>
2021-01-26 11:37:17 +0900

    .

--- 백준알고리즘풀이/algo2110.java
+++ 백준알고리즘풀이/algo2110.java
@@ -1,47 +1,50 @@
-package algo;
-import java.util.*;
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class algo2110 {
+    static ArrayList<Integer> arr;
+    static int n,m;
+    static int max;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        n=Integer.parseInt(str[0]);
+        m=Integer.parseInt(str[1]);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        arr = new ArrayList<>();
+        for (int i=0;i<n;i++)arr.add(Integer.parseInt(br.readLine()));
+        Collections.sort(arr);
 
-		Scanner scan=new Scanner(System.in);
-		
-		ArrayList map=new ArrayList();
-		
-		int n=scan.nextInt();int m=scan.nextInt();
-		int house[]=new int[n];
-		for(int i=0;i<n;i++)house[i]=scan.nextInt();
-		Arrays.sort(house);
-		
-		int left=house[0]; int right=house[n-1]-house[0];
-		int mid=0;
-		
-		while(left<=right) {
-			int cnt=1, prev=house[0];
-			mid=(left+right)/2;
-			
-			for(int h:house) {
-				if(h-prev>=mid) {
-					cnt++;prev=h;
-				}
-			}
-			
-			if(cnt>=m)left=mid+1;
-			else right=mid-1;
-			
-		}
-		System.out.println(right);
-		
-		
-		
-		
-//		for(int i=0;i<tot.size();i++) {
-//			ArrayList a=(ArrayList)tot.get(i);
-//			for(int j=0;j<a.size();j++) {
-//				System.out.printf("%d ", a.get(j));
-//			}System.out.println("");
-//		}
-	}
+        int left = 1; int right = arr.get(arr.size()-1);
+        max=0;
+
+        while (left<=right){
+            int mid = (left+right)/2;
+            int start = arr.get(0);
+            int dis = 0;
+            int cnt =1;
+
+            for (int i=1;i<n;i++){
+                dis = arr.get(i) - start;
+                if (dis>=mid){
+                    cnt++;
+                    start = arr.get(i);
+                }
+            }
+            if (cnt>=m){
+                max = mid;
+                left = mid + 1;
+            }else {
+                right = mid -1;
+            }
+
+        }
+        System.out.println(max);
+    }
 
 }
