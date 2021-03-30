9117ff96d5a0c1d7f77d25ed13d0dddd37dc1b9d
변성재 <dfdo333@naver.com>
2021-03-30 11:46:04 +0900

    ,

--- 백준알고리즘풀이/algo11049.java
+++ 백준알고리즘풀이/algo11049.java
@@ -1,44 +1,39 @@
-package algo;
-import java.util.*;
-import java.io.*;
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class algo11049 {
+    static int[][] map;
+    static int n;
+    static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(br.readLine());
+        map=new int[n][2];
+        dp=new int[n][n];
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
+        for (int i=0;i<n;i++){
+            String[] str=br.readLine().split(" ");
+            map[i][0]=Integer.parseInt(str[0]);
+            map[i][1]=Integer.parseInt(str[1]);
+        }
+        System.out.println(find(0,n-1));
 
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-		int n=Integer.parseInt(br.readLine());
-		
-		int dp[]=new int[n+1];
-		int s[]=new int[n+1];
-		int e[]=new int[n+1];
-		
-		for(int i=1;i<=n;i++) {
-			String in[]=br.readLine().split(" ");
-			s[i]=Integer.parseInt(in[0]);
-			e[i]=Integer.parseInt(in[1]);
-			
-		}
-		
-		
-		for(int i=1;i<=n;i++) {
-			
-			
-			
-			
-			
-			
-			
-			
-		}
-		
-		
-		
-		
-		
-		
-		
-	}
+    }
+    static int find(int a,int b){
+        if (a==b)return 0;
+        if (dp[a][b]!=0)return dp[a][b];
 
+        int min =Integer.MAX_VALUE;
+
+        for (int i=a;i<b;i++){
+            min = Math.min(min,find(a,i)+find(i+1,b)+map[a][0]*map[i][1]*map[b][1]);
+        }
+
+        dp[a][b]=min;
+        return min;
+    }
 }
