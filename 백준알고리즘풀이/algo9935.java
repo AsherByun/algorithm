c496ad01ca1e643900e99eaf41e28aee6f11a463
변성재 <dfdo333@naver.com>
2021-05-07 14:14:42 +0900

    문자열폭발

--- 백준알고리즘풀이/algo9935.java
+++ 백준알고리즘풀이/algo9935.java
@@ -1,58 +1,41 @@
-package algo;
-import java.util.*;
-import java.io.*;
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class algo9935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String tar = br.readLine();
+        int n = tar.length();
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+        char[] cmap=tar.toCharArray();
+        int idx=0;
+        char[] out=new char[str.length()];
 
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-		String in=br.readLine();
-		String bm=br.readLine();
-		
-		ArrayList map=new ArrayList();
-		Stack s=new Stack();
-		
-		for(int i=0;i<in.length();i++) {
-			map.add(in.charAt(i));
-			if(in.charAt(i)==bm.charAt(0)) {
-				s.add(i);
-			}
-		}
-		int tsize=map.size();
-		int np=0;
-		
-		while(!s.isEmpty()) {
-			
-			if(map.size()<bm.length())break;
-			
-			int p=(int)s.pop();
-			boolean bc=true;
-			
-			if(p+bm.length()<=map.size()) {
-			for(int i=0;i<bm.length();i++) {
-				
-				if(bm.charAt(i)!=(char)map.get(p+i)) {
-					bc=false;
-				}
-			}
-			if(bc) {
-				for(int i=0;i<bm.length();i++) {
-					map.remove(p);
-				}
-			}
-			}
-			
-			
-		}
-		if(map.size()>0) {
-		for(int i=0;i<map.size();i++)System.out.printf("%c", map.get(i));
-		}else {
-			System.out.println("FRULA");
-		}
-		
-	}
+        for (int i=0;i<str.length();i++){
+            out[idx] = str.charAt(i);
+            if (check(out,idx,tar))idx -= tar.length();
+            idx++;
+        }
+        String ans=String.valueOf(out,0,idx);
 
+        if (ans.equals(""))System.out.println("FRULA");
+        else System.out.println(ans);
+    }
+
+    static boolean check(char[] out,int idx,String bomb){
+        if (idx<bomb.length()-1)return false;
+
+        for (int i=0;i<bomb.length();i++){
+            if(bomb.charAt(i)!=out[idx-bomb.length()+1+i])return false;
+        }
+        return true;
+    }
 }
